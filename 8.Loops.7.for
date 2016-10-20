6e6a7abb072a8e05c2c28ba339b0b777adec42b3
C.N <claire_nguyen@outlook.com>
2016-10-20 16:14:24 -0400

    Create 8.Loops.7.for

---
+++ 8.Loops.7.for
@@ -0,0 +1,7 @@
+'''
+For your health
+An alternative way to loop is the for loop. The syntax is as shown; this example means "for each number i in the range 0 - 9, print i".
+'''
+print "Counting: for i in range(10):"
+for i in range(10):
+    print i
