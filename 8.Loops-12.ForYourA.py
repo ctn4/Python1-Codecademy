136314f9fe639450c45ce0fc07b58b131394c2c4
C.N <claire_nguyen@outlook.com>
2016-10-20 16:46:40 -0400

    Create 8.Loops-12.ForYourA.py

---
+++ 8.Loops-12.ForYourA.py
@@ -0,0 +1,10 @@
+
+phrase = 'A bird in the hand ...'
+# loop
+for char in phrase:
+  if char == 'A' or char == 'a':
+    print 'X', # the comma after the print statement means keep printing on the same line
+  else:
+    print char, # the comma here, again..
+# From the course: "Don't delete this print statement" ...    
+print
