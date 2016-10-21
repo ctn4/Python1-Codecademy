db69300fa77df693e2a2ccb257692cf1d22ca30e
C.N <claire_nguyen@outlook.com>
2016-10-21 10:32:03 -0400

    Update 8.Loops.14.LoopOverADictionary.py

--- 8.Loops.14.LoopOverADictionary.py
+++ 8.Loops.14.LoopOverADictionary.py
@@ -1,3 +1,6 @@
+'''
+File: CN-codedemy-script-8.Loops-14.LoopOverADictionary.py
+'''
 d = {'a': 'apple', 'b': 'berry', 'c': 'cherry', 'p': 'pear', 'o': 'orange'}
 for key in d:
   print key, d[key] # Note: this is how to access a key then its value in the dictionary
