5345f1b8e044d807c9f50c6c8c5e06c1237f1ab0
C.N <claire_nguyen@outlook.com>
2016-10-21 12:12:11 -0400

    Create 8.Loops.19.CreateOwn.py

---
+++ 8.Loops.19.CreateOwn.py
@@ -0,0 +1,18 @@
+'''
+8.Loops.19.Create Own For/Else
+
+File: CN-codedemy-script-8.Loops.19.CreateOwn.py
+Oct 21, 2016: worked
+Tested 2 cases:
+songs = ['Happy Together', 'Que Sera Sera', 'Hey Jude'] -> output" Hey Jude
+songs = ['Happy Together', 'Que Sera Sera'] -> output: I like all songs ['Happy Together', 'Que Sera Sera']
+'''
+songs = ['Happy Together', 'Que Sera Sera', 'Hey Jude']
+# songs = ['Happy Together', 'Que Sera Sera']
+
+for song in songs:
+  if song == 'Hey Jude'
+    print song
+    break
+else:
+  print 'I like all songs. I don\'t like Hey Jude', songs
